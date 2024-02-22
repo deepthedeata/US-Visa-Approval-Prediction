2fbcde295c8f3c1efd661e9f5060f99ee84f70e8
deepthedeata <deependrasingh756@gmail.com>
2024-02-22 23:30:53 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # US-Visa-Approval-Prediction
+
+## git commands
+
+git add .
+git commit -m "Updated"
+git push orgin main
--- template.py
+++ template.py
@@ -20,6 +20,8 @@ list_of_files = [
     f"{project_name}/pipeline/__init__.py",
     f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
     "app.py",
     "requirements.txt",
     "dockerignore",
