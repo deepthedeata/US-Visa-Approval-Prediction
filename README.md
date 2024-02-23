c530a282512aafd05826d6731dc943889af39137
deepthedeata <deependrasingh756@gmail.com>
2024-02-23 09:54:15 +0530

    requirements added

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 git add .
 git commit -m "Updated"
 git push orgin main
+....
+bash
+conda create -n visa python=3.8 -y
+...
+bash
+conda activate visa
+...
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Deependra singh",
+    author_email="deependrasingh756@gmail.com",
+    packages=find_packages()
+)
